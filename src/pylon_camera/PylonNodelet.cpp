de44fde3254014e67edf1e3737748ba5c4573f2f
rdiederichse <rdiederichse@uos.de>
2017-07-07 14:10:27 +0200

    Fix class name + move into pylon_camera namespace

--- src/pylon_camera/PylonNodelet.cpp
+++ src/pylon_camera/PylonNodelet.cpp
@@ -7,34 +7,35 @@
 #include <pylon_camera/pylon_camera_node.h>
 #include <boost/bind.hpp>
 
-using pylon_camera::PylonCameraNode;
-
-class PanoramaPylonNodelet : public nodelet::Nodelet
+namespace pylon_camera
 {
-    private:
-        pylon_camera::PylonCameraNode pylon_camera_node;
+    class PylonNodelet : public nodelet::Nodelet
+    {
+        private:
+            PylonCameraNode pylon_camera_node;
 
-        void spinPylon()
-        {
-            // poll the camera infinitely
-            ros::Rate rate(30);
-            while (ros::ok())
+            void spinPylon()
             {
-                pylon_camera_node.spin();
-                rate.sleep();
+                // poll the camera infinitely
+                ros::Rate rate(30);
+                while (ros::ok())
+                {
+                    pylon_camera_node.spin();
+                    rate.sleep();
+                }
             }
-        }
 
-    public:
-        virtual void onInit()
-        {
-            NODELET_INFO_STREAM("Start image grabbing if node connects to topic with "
-                    << "a frame_rate of: " << pylon_camera_node.frameRate() << " Hz");
+        public:
+            virtual void onInit()
+            {
+                NODELET_INFO_STREAM("Start image grabbing if node connects to topic with "
+                        << "a frame_rate of: " << pylon_camera_node.frameRate() << " Hz");
 
-            // Main thread and brightness-service thread
-            boost::thread th(boost::bind(&PanoramaPylonNodelet::spinPylon, this));
-        }
+                // Main thread and brightness-service thread
+                boost::thread th(boost::bind(&PylonNodelet::spinPylon, this));
+            }
 
-};
+    };
 
-PLUGINLIB_EXPORT_CLASS(PanoramaPylonNodelet, nodelet::Nodelet)
+}
+PLUGINLIB_EXPORT_CLASS(pylon_camera::PylonNodelet, nodelet::Nodelet)
